df9bd417773ccfa29af96c736f4ca094ab58b29f
Ricerolls64 <chen65richard@gmail.com>
2022-09-21 17:11:12 -0500

    1.0.7

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const log = console.log;
 let player, tables, kts, aps, dts;
+let food, burntFood;
 
 let speed = 2.5;
 let keyHeldCount = 0;
@@ -11,12 +12,16 @@ let dtOffset = dtSize * 1.05;
 let dtAmount = 8;
 
 let dish = [];
+let inventory;
 let score = 0;
 let tableNum, recipe;
+let spriteBoxOffset = 10;
+
+let bell;
 
 // i3 is cookie
 let recipes = {
-	i2: [['i0', 'i1'], 'a0']
+	cookie: [['dough', 'chocolate'], 'oven']
 };
 
 // keys are the item name, values are the cooking time
@@ -25,31 +30,45 @@ let cooking = {};
 function oven() {
 	for (let item in cooking) {
 		if (cooking[item] < 0) {
+			if (cooking[item] < -200) {
+				delete cooking[item];
+				return 'burnt ' + item;
+			}
 			delete cooking[item];
 			return item;
 		}
-		// TODO if it cooked for too long give them burnt item
 	}
-	if (dish.includes('i0') && dish.includes('i1')) {
+	if (dish.includes('dough') && dish.includes('chocolate')) {
 		// await delay(5000)
-		let i = dish.indexOf('i0');
+		let i = dish.indexOf('dough');
 		dish.splice(i, 1);
-		i = dish.indexOf('i1');
+		inventory[i].remove();
+		i = dish.indexOf('chocolate');
 		dish.splice(i, 1);
-		cooking.i2 = 200;
+		inventory[i].remove();
+		cooking.cookie = 200;
 	}
 }
 
 function preload() {
 	food = new Group();
-	food.spriteSheet = loadImage('food0.png');
+	food.spriteSheet = loadImage('img/food0.png');
 	food.tileSize = 16;
-	food.addAnis({
+	let atlas = {
 		cookie: [0, 0],
 		chocolate: [1, 0],
 		dough: [5, 0]
-	});
+	};
+	food.addAnis(atlas);
 	food.collider = 'none';
+
+	burntFood = new Group();
+	burntFood.spriteSheet = loadImage('img/food1.png');
+	burntFood.tileSize = 16;
+	burntFood.addAnis(atlas);
+	burntFood.collider = 'none';
+
+	bell = loadSound('sounds/bell.wav');
 }
 
 function setup() {
@@ -64,8 +83,8 @@ function setup() {
 
 	tableColliders.overlap(tables);
 
-	garbage = new tables.Sprite(20, 192, 24);
-	new tableColliders.Sprite(20, 192, 12);
+	garbage = new tables.Sprite(10, 90, 24);
+	new tableColliders.Sprite(10, 90, 12);
 
 	// kitchen tables with ingredients
 	kts = new tables.Group();
@@ -74,10 +93,13 @@ function setup() {
 
 	// x, y, w, h
 	new kts.Sprite(88, 26, 30, 40);
-	new tableColliders.Sprite(88, 26, 18, 26);
-
 	new kts.Sprite(88, 88, 57, 35);
-	new tableColliders.Sprite(88, 88, 44, 22);
+	new kts.Sprite(160, 28, 50, 40);
+	new kts.Sprite(150, 81, 40, 42);
+
+	for (let kt of kts) {
+		new tableColliders.Sprite(kt.x, kt.y, kt.w - 12, kt.h - 12);
+	}
 
 	new aps.Sprite(10, 140, 35, 40);
 	new tableColliders.Sprite(10, 140, 20, 70);
@@ -99,19 +121,39 @@ function setup() {
 
 	player = new Sprite(22, 22, 16);
 
+	food.tileSize = 1;
+	burntFood.tileSize = 1;
+
+	inventory = new Group();
+	let ingredients = ['dough', 'chocolate', 'cookie'];
 	for (let i = 0; i < kts.length; i++) {
 		player.overlap(kts[i], () => {
-			if (dish.includes('i' + i) == false) {
-				dish.push('i' + i);
+			if (dish.includes(ingredients[i]) == false) {
+				dish.push(ingredients[i]);
 				log(dish);
+				inventory.push(new food.Sprite(ingredients[i], inventory.length * 20 + spriteBoxOffset, 191));
 			}
 		});
 	}
 
+	// oven
 	player.overlap(aps[0], () => {
-		let result = oven();
-		if (result != undefined) {
-			dish.push(result);
+		let foodName = oven();
+
+		if (foodName != undefined) {
+			let group;
+			if (!foodName.includes('burnt')) {
+				group = food;
+			} else {
+				group = burntFood;
+				foodName = foodName.slice(6);
+				log(foodName);
+			}
+			let res = new group.Sprite(foodName, inventory.length * 20 + spriteBoxOffset, 192);
+
+			dish.push(foodName);
+			inventory.push(res);
+
 			log(dish);
 		}
 	});
@@ -121,11 +163,10 @@ function setup() {
 	player.overlap(garbage, () => {
 		dish = [];
 		log(dish);
+		inventory.remove();
 	});
 
-	food.tileSize = 1;
-	let ingredients = ['dough', 'chocolate'];
-	for (let i = 0; i < ingredients.length; i++) {
+	for (let i = 0; i < kts.length; i++) {
 		new food.Sprite(ingredients[i], kts[i].x, kts[i].y);
 	}
 
@@ -165,16 +206,25 @@ function nextDish() {
 function draw() {
 	background(128);
 
+	fill(255);
 	textAlign(LEFT);
 	text('Score: ' + score, 10, 20);
 	text('Table: ' + tableNum, 10, 40);
 	text('Recipie: ' + recipe, 10, 60);
+
+	fill(0, 0, 0, 0);
+	for (let i = 0; i < inventory.length; i++) {
+		rect(i * 20, 180, 20, 20);
+	}
 
 	// log(frameCount); // in game frames drawn
 	// log(Date.now()); // real world time
 	for (let item in cooking) {
 		cooking[item]--;
 		log(item, cooking[item]);
+		if (cooking[item] == 0) {
+			bell.play();
+		}
 	}
 
 	let keysDown = 0;
