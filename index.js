fafc530c00947247f9f515a0c9c41d59bf592198
Ricerolls64 <chen65richard@gmail.com>
2023-01-27 18:27:00 -0600

    1.0.10

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ let spdIncr = 0.3;
 let slushSpd = 1;
 let dtSize = 48;
 let dtOffset = dtSize * 1.05;
-let dtAmount = 8;
+let dtAmount = 6;
 
 let ingredients;
 let dish = [];
@@ -20,22 +20,83 @@ let spriteBoxOffset = 10;
 let ovenAvailable = true;
 
 let bell;
+let no;
 
-// i3 is cookie
-let recipes = {
-	bread: [['dough'], 'oven'],
-	cookie: [['dough', 'chocolate'], 'oven'],
-	applePie: [['apple', 'dough'], 'oven'],
-	cheeseCake: [['cheese', 'dough'], 'oven'],
-	roastedHam: [['pig'], 'oven'],
-	grilledSalmon: [['salmon'], 'stove'],
-	boiledShrimp: [['shrimp'], 'stove']
-};
+let recipes = [
+	{
+		name: 'applePie',
+		ingredients: ['apple', 'dough'],
+		appliance: 'oven',
+		time: 400
+	},
+	{
+		name: 'cheeseCake',
+		ingredients: ['cheese', 'dough'],
+		appliance: 'oven',
+		time: 400
+	},
+	{
+		name: 'cookie',
+		ingredients: ['dough', 'chocolate'],
+		appliance: 'oven',
+		time: 200
+	},
+	{
+		name: 'bread',
+		ingredients: ['dough'],
+		appliance: 'oven',
+		time: 200
+	},
+	{
+		name: 'boiledShrimp',
+		ingredients: ['shrimp'],
+		appliance: 'stove',
+		time: 200
+	},
+	{
+		name: 'grilledSalmon',
+		ingredients: ['salmon'],
+		appliance: 'stove',
+		time: 200
+	},
+	{
+		name: 'roastedHam',
+		ingredients: ['pig'],
+		appliance: 'oven',
+		time: 400
+	}
+];
 
 // keys are the item name, values are the cooking time
 let cooking = {};
 
 function oven() {
+	// only cook something in the oven if it's available
+	if (ovenAvailable) {
+		for (let recipe of recipes) {
+			if (recipe.appliance != 'oven') continue; // skip!
+
+			// check if the player has the ingredients to make the item
+			let canMakeRecipe = true;
+			for (let item of recipe.ingredients) {
+				if (!dish.includes(item)) {
+					canMakeRecipe = false;
+				}
+			}
+			if (!canMakeRecipe) continue; // skip
+
+			ovenAvailable = false;
+			for (let item of recipe.ingredients) {
+				removeItemFromInventory(item);
+			}
+
+			cooking[recipe.name] = recipe.time;
+		}
+	}
+
+	if (inventory.length >= 5) return; // exit function
+
+	// remove item from oven
 	for (let item in cooking) {
 		if (cooking[item] < 0) {
 			ovenAvailable = true;
@@ -47,23 +108,18 @@ function oven() {
 			return item;
 		}
 	}
-
-	if (ovenAvailable == false) return;
-
-	if (dish.includes('dough') && dish.includes('chocolate')) {
-		ovenAvailable = false;
-		// await delay(5000)
-		removeItemFromInventory('dough');
-		removeItemFromInventory('chocolate');
-		cooking.cookie = 200;
-	}
 }
 
 function removeItemFromInventory(item) {
 	let i = dish.indexOf(item);
+	if (i == -1) {
+		no.play();
+		return false;
+	}
 	dish.splice(i, 1);
 	inventory[i].remove();
 	repoInventory();
+	return true;
 }
 
 function repoInventory() {
@@ -74,9 +130,12 @@ function repoInventory() {
 
 function serveToTable(i) {
 	if (i == tableNum) {
-		removeItemFromInventory('cookie');
-		score++;
-		nextDish();
+		let result = removeItemFromInventory(recipe.name);
+		order.removeAll();
+		if (result) {
+			score++;
+			nextDish();
+		}
 	}
 }
 
@@ -85,13 +144,14 @@ function preload() {
 	food.spriteSheet = loadImage('img/food0.png');
 	food.tileSize = 16;
 	let atlas = {
-		cookie: [0, 0],
-		chocolate: [1, 0],
-		dough: [5, 0],
 		apple: [4, 1],
 		applePie: [4, 4],
+		bread: [2, 3],
 		cheese: [0, 3],
 		cheeseCake: [5, 4],
+		chocolate: [1, 0],
+		cookie: [0, 0],
+		dough: [5, 0],
 		pig: [1, 1],
 		roastedHam: [1, 5],
 		salmon: [7, 2],
@@ -108,7 +168,10 @@ function preload() {
 	burntFood.addAnis(atlas);
 	burntFood.collider = 'none';
 
+	order = new Group();
+
 	bell = loadSound('sounds/bell.wav');
+	no = loadSound('sounds/Laser_Shoot.wav');
 }
 
 function setup() {
@@ -185,10 +248,21 @@ function setup() {
 	nextDish();
 }
 
+function displayRecipe() {
+	for (let i = 0; i < recipe.ingredients.length; i++) {
+		let item = recipe.ingredients[i];
+		order.push(new food.Sprite(item, width - i * 20 - spriteBoxOffset, 191));
+	}
+}
+
 function getIngredient(i) {
-	dish.push(ingredients[i]);
-	log(dish);
-	inventory.push(new food.Sprite(ingredients[i], inventory.length * 20 + spriteBoxOffset, 191));
+	if (inventory.length < 5) {
+		dish.push(ingredients[i]);
+		log(dish);
+		inventory.push(new food.Sprite(ingredients[i], inventory.length * 20 + spriteBoxOffset, 191));
+	} else {
+		no.play();
+	}
 }
 
 function nextDish() {
@@ -196,10 +270,11 @@ function nextDish() {
 	let recipeNum = round(random(0, 6));
 
 	let table = dts[tableNum];
-	recipe = recipes[Object.keys(recipes)[recipeNum]];
+	recipe = recipes[recipeNum];
 
 	log('table: ' + tableNum);
-	log('recipe: ' + recipe);
+	log('recipe: ' + recipe.name + ' ingredients: ' + recipe.ingredients + ' appliance: ' + recipe.appliance);
+	displayRecipe();
 }
 
 function draw() {
@@ -208,12 +283,19 @@ function draw() {
 	fill(255);
 	textAlign(LEFT);
 	text('Score: ' + score, 10, 20);
-	text('Table: ' + tableNum, 10, 40);
+	text('Table: ' + tableNum, 315, 175);
 
-	fill(0, 0, 0, 0);
-	for (let i = 0; i < inventory.length; i++) {
+	// draw inventory item boxes
+	fill(100);
+	for (let i = 0; i < 5; i++) {
 		rect(i * 20, 180, 20, 20);
 	}
+	fill(255);
+	for (let i = 1; i <= recipe.ingredients.length; i++) {
+		rect(width - i * 20, 180, 20, 20);
+	}
+
+	stroke(0);
 
 	// log(frameCount); // in game frames drawn
 	// log(Date.now()); // real world time
@@ -313,19 +395,5 @@ function draw() {
 
 	if (keysDown == 0) {
 		keyHeldCount = 0;
-	}
-
-	allSprites.draw();
-
-	textAlign(CENTER);
-
-	// for (let i = 0; i < kts.length; i++) {
-	// 	let kt = kts[i];
-	// 	text('kt' + i, kt.x, kt.y);
-	// }
-
-	for (let i = 0; i < dts.length; i++) {
-		let dt = dts[i];
-		text('dt' + i, dt.x, dt.y);
 	}
 }
